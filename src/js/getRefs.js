6f658d1793dc0148049d559b79b67d0de4e95615
IHOR <97735057+ihoretti@users.noreply.github.com>
2022-10-17 23:07:24 +0300

    Create getRefs.js

---
+++ src/js/getRefs.js
@@ -0,0 +1,9 @@
+export function getRefs() {
+  return {
+    searchField: document.querySelector('#search-form'),
+    getGallery: document.querySelector('.gallery'),
+    searchBtn: document.querySelector('#button-submit'),
+    // getGalleryLink: document.querySelectorAll('.gallery__link')
+    // loadMoreBtn: document.querySelector('.load-more'),
+  };
+}
